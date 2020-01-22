872a18f2d87e5094d4818ef9cf766f0e239a695a
mkkiyoi <mkkiyoi@gmail.com>
2020-01-22 14:34:14 -0800

    Updates

---
+++ precache-manifest.19f6b9bc117d238bb9c6097572e27ec0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "667257e5daac27f0eddd0b130e8826f6",
+    "url": "/Memory-Game/index.html"
+  },
+  {
+    "revision": "c5fa04739e210e3c3636",
+    "url": "/Memory-Game/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "cc8863cd0c4dbb8c1450",
+    "url": "/Memory-Game/static/css/main.f5ff8e6a.chunk.css"
+  },
+  {
+    "revision": "c5fa04739e210e3c3636",
+    "url": "/Memory-Game/static/js/2.340ca04b.chunk.js"
+  },
+  {
+    "revision": "f032203ca460334c00de541c30a6078a",
+    "url": "/Memory-Game/static/js/2.340ca04b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "cc8863cd0c4dbb8c1450",
+    "url": "/Memory-Game/static/js/main.605e054f.chunk.js"
+  },
+  {
+    "revision": "68bd510a1b1ecb7f4194",
+    "url": "/Memory-Game/static/js/runtime-main.fa260db5.js"
+  }
+]);
